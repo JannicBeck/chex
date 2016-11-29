91f9966fcdd3360d3f669f3621e7f0b2ce0e74ad
Jannic Beck <jb@MacBook-Pro.fritz.box>
2016-11-29 22:54:40 +0100

    getting started with reducers

--- index.ts
+++ index.ts
@@ -1,2 +1,23 @@
 import { createStore } from 'redux';
-console.log(createStore);
+
+const MOVE = 'MOVE';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case MOVE:
+      return state;
+    default:
+      return state;
+  }
+};
+
+let store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({type: MOVE, payload: {
+  from: 'e7',
+  to: 'e6'
+}});
