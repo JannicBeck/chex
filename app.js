85e615482b2752b7afc6d5545c641b156862620e
Jannic Beck <jb@Jannics-MBP.fritz.box>
2016-12-03 16:47:55 +0100

    refactored coloring code

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
 const Rx = require('redux');
 const chalk = require('chalk');
 
+
 const logBoard = (board) => {
-
-  let b = board.map((square, index) => {
-    square = square ? square.player.charAt(0) + square.type.charAt(0) : '  ';
-    let rowIndex = Math.floor(index / 8);
-    let squareEven = index % 2 === 0;
-    let rowEven = rowIndex % 2 !== 0;
-    let isWhite = square.charAt(0) === 'W';
-
-    if ((!squareEven && rowEven) || (squareEven && !rowEven)) {
-      square = chalk.bgWhite(square);
-    } else {
-      square = chalk.bgBlack(square);
-    }
-    if (square) {
-      if (isWhite) {
-        square = chalk.white(square);
-      } else {
-        square = chalk.grey(square);
-      }
-    }
-    return square;
-  });
-
-  for (let i = 8; i <= b.length; i += 8) {
-    let row = b.slice(i - 8, i);
+  let coloredBoard = board
+    .map(colorText)
+    .map(colorBg);
+  for (let i = 8; i <= coloredBoard.length; i += 8) {
+    let row = coloredBoard.slice(i - 8, i);
     console.log(chalk.black(row));
   }
-
   console.log('\n');
 };
+
+const colorText = (square) => {
+  if (square) {
+    let figure = square.player.charAt(0) + square.type.charAt(0);
+    if (square.player === Player.White) {
+      return chalk.white(figure);
+    } else {
+      return chalk.grey(figure);
+    }
+  } else {
+    return '\u0020\u0020';
+  }
+};
+
+const colorBg = (square, index) => {
+  let rowIndex = Math.floor(index / 8);
+  let squareEven = index % 2 === 0;
+  let rowEven = rowIndex % 2 !== 0;
+  if ((!squareEven && rowEven) || (squareEven && !rowEven)) {
+    return chalk.bgWhite(square);
+  } else {
+    return chalk.bgBlack(square);
+  }
+};
+
 
 const Pos = {
   A1: 0, B1: 1, C1: 2, D1: 3, E1: 4, F1: 5, G1: 6, H1: 7,
