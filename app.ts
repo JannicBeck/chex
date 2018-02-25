fad116c043b2bbe7b6dd05e8625feb3fa97da972
Jannic Beck <jannicbeck@gmail.com>
2018-02-25 19:48:36 +0100

    Start positions at 21 to conform with the 12x10 board representation

--- app.ts
+++ app.ts
@@ -43,15 +43,18 @@ const range = (start: number, end: number, xs: ReadonlyArray<number> = []): Read
 
 const isNumber = complement(Number.isNaN)
 
+// | A1 is at 21 since there are two rows above the board
+// * and one row on each side of the board, so we know when the knight
+// * is jumping off the board
 enum Pos {
-  A8, B8, C8, D8, E8, F8, G8, H8,
-  A7, B7, C7, D7, E7, F7, G7, H7,
-  A6, B6, C6, D6, E6, F6, G6, H6,
-  A5, B5, C5, D5, E5, F5, G5, H5,
-  A4, B4, C4, D4, E4, F4, G4, H4,
-  A3, B3, C3, D3, E3, F3, G3, H3,
-  A2, B2, C2, D2, E2, F2, G2, H2,
-  A1, B1, C1, D1, E1, F1, G1, H1
+  A8 = 21, B8, C8, D8, E8, F8, G8, H8,
+  A7 = 31, B7, C7, D7, E7, F7, G7, H7,
+  A6 = 41, B6, C6, D6, E6, F6, G6, H6,
+  A5 = 51, B5, C5, D5, E5, F5, G5, H5,
+  A4 = 61, B4, C4, D4, E4, F4, G4, H4,
+  A3 = 71, B3, C3, D3, E3, F3, G3, H3,
+  A2 = 81, B2, C2, D2, E2, F2, G2, H2,
+  A1 = 91, B1, C1, D1, E1, F1, G1, H1
 }
 
 const PIECE_SYMBOLS = {
